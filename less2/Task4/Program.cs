29509925ab65efca1f93d5097025de801ec2c287
Dmitry <noisn@mail.ru>
2024-04-13 13:59:47 +0300

    create new info

---
+++ less2/Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = 5;
+int[] array = { 2, 5, 6, 9, 7 };
+int i = 0;
+int max = array[0];
+while(i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
+
+
+max = array[0];
+for(int j = 0; j < n; j++)// j В данном случае счетчик вместо i
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array )// Меняется только способ обращения к элементу массива
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine(max);
