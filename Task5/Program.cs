9a5343735a638ea990bce9e0a05a9492acf0aaf9
Dmitry <noisn@mail.ru>
2024-04-13 12:52:47 +0300

    add new files

---
+++ Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 9;
+int b = 10;
+int c = 4;
+int d = 1;
+int e = 6;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
