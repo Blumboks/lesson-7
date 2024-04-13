af44fad1e56e72c35fe555801c7ee99530b4e073
Dmitry <noisn@mail.ru>
2024-04-13 13:32:40 +0300

    create new info

---
+++ less2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 1, 4, 5, 6, 7, 8, 9, 5, 77, 88 };
+int i = 0;
+
+while(i < n)
+{
+    if(array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
